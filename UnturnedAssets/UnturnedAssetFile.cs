7f6d0c9767d9c7355a4af06d5d86f109356e905a
Danny Willett <dannywill8@gmail.com>
2023-10-15 00:55:06 -0400

    Add project files.

---
+++ UnturnedAssets/UnturnedAssetFile.cs
@@ -0,0 +1,134 @@
+﻿using SDG.Unturned;
+using System.Globalization;
+
+namespace UnturnedAssets;
+public class UnturnedAssetFile
+{
+    private bool _cachedLocal;
+    private bool _cachedFriendlyName;
+    private DatDictionary? _local;
+    private string? _friendlyName;
+    public string AssetName { get; }
+    public string? LocalPath { get; }
+    public FileInfo File { get; }
+    public DatDictionary Dictionary { get; }
+    public Type AssetType { get; }
+    public Guid Guid { get; }
+    public ushort Id { get; }
+    public EAssetType Category { get; }
+
+    public string? FriendlyName
+    {
+        get
+        {
+            if (_cachedFriendlyName)
+                return _friendlyName!;
+            DatDictionary? lcl = Local;
+            if (lcl != null)
+                lcl.TryGetString("Name", out _friendlyName);
+            _cachedFriendlyName = true;
+            return _friendlyName;
+        }
+    }
+
+    public string FriendlyNameOrAssetName => FriendlyName ?? AssetName;
+
+    public DatDictionary? Local
+    {
+        get
+        {
+            if (!_cachedLocal)
+                CacheLocal(null);
+            return _local;
+        }
+    }
+    public UnturnedAssetFile(Type assetType, FileInfo file, Guid guid, ushort id, DatDictionary dictionary, string language = "English")
+    {
+        AssetType = assetType;
+        File = file;
+
+        // if file name is 'Asset.dat', get the asset name from the parent folder.
+        if (file.FullName.EndsWith("Asset.dat", StringComparison.Ordinal))
+            AssetName = Path.GetFileName(Path.GetDirectoryName(file.FullName)!);
+        else
+            AssetName = Path.GetFileNameWithoutExtension(file.FullName);
+
+        Dictionary = dictionary;
+        Guid = guid;
+        Id = id;
+        string lclPath = Path.Combine(Path.GetDirectoryName(File.FullName)!, language + ".dat");
+
+        // English.dat file
+        if (System.IO.File.Exists(lclPath))
+            LocalPath = lclPath;
+        else if (language == null || !language.Equals("English", StringComparison.Ordinal))
+            LocalPath = Path.Combine(Path.GetDirectoryName(File.FullName)!, "English.dat");
+
+        // category from asset type
+        if (typeof(ItemAsset).IsAssignableFrom(assetType))
+            Category = EAssetType.ITEM;
+        else if (typeof(EffectAsset).IsAssignableFrom(assetType))
+            Category = EAssetType.EFFECT;
+        else if (typeof(VehicleAsset).IsAssignableFrom(assetType))
+            Category = EAssetType.VEHICLE;
+        else if (typeof(ObjectAsset).IsAssignableFrom(assetType))
+            Category = EAssetType.OBJECT;
+        else if (typeof(ResourceAsset).IsAssignableFrom(assetType))
+            Category = EAssetType.RESOURCE;
+        else if (typeof(AnimalAsset).IsAssignableFrom(assetType))
+            Category = EAssetType.ANIMAL;
+        else if (typeof(MythicAsset).IsAssignableFrom(assetType))
+            Category = EAssetType.MYTHIC;
+        else if (typeof(SkinAsset).IsAssignableFrom(assetType))
+            Category = EAssetType.SKIN;
+        else if (typeof(SpawnAsset).IsAssignableFrom(assetType))
+            Category = EAssetType.SPAWN;
+        else if (typeof(DialogueAsset).IsAssignableFrom(assetType) || typeof(VendorAsset).IsAssignableFrom(assetType) || typeof(QuestAsset).IsAssignableFrom(assetType))
+            Category = EAssetType.NPC;
+        else Category = EAssetType.NONE;
+    }
+    internal void CacheLocal(DatParser? parser = null)
+    {
+        _cachedLocal = true;
+        if (LocalPath == null || !System.IO.File.Exists(LocalPath))
+            return;
+        parser ??= new DatParser();
+        using FileStream fileStream = new FileStream(LocalPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+        using StreamReader inputReader = new StreamReader(fileStream);
+
+        lock (parser)
+            _local = parser.Parse(inputReader);
+    }
+    internal async Task CacheLocalAsync(DatParser? parser = null, CancellationToken token = default)
+    {
+        _cachedLocal = true;
+        if (LocalPath == null || !System.IO.File.Exists(LocalPath))
+            return;
+        parser ??= new DatParser();
+#if NETCOREAPP || NETSTANDARD
+        await using FileStream fileStream = new FileStream(LocalPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+#else
+        using FileStream fileStream = new FileStream(LocalPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+#endif
+
+        if (fileStream.Length > UnturnedAssetFinder.MaxFileSize)
+            return;
+
+        using MemoryStream stream = new MemoryStream((int)fileStream.Length);
+        await fileStream.CopyToAsync(stream, 81920, token).ConfigureAwait(false);
+        stream.Seek(0L, SeekOrigin.Begin);
+
+        using StreamReader inputReader = new StreamReader(stream);
+        
+        lock (parser)
+            _local = parser.Parse(inputReader);
+    }
+
+    public override string ToString()
+    {
+        if (Id != 0)
+            return Id.ToString(CultureInfo.InvariantCulture) + " | " + AssetName + " | " + AssetType.Name;
+        
+        return AssetName + " | " + AssetType.Name;
+    }
+}
---
+++ UnturnedAssets/UnturnedAssetFinder.cs
@@ -0,0 +1,256 @@
+﻿using Microsoft.Extensions.Logging;
+using SDG.Unturned;
+
+namespace UnturnedAssets;
+public class UnturnedAssetFinder : IDisposable
+{
+    internal const int MaxFileSize = 8388608; // 8MB
+
+    private readonly DatParser _parser = new DatParser();
+    private readonly ILogger<UnturnedAssetFinder> _logger;
+    private static UnturnedNexus? _nexus;
+    private static volatile int _semaphoreQueue;
+    private volatile int _disposed;
+
+    public UnturnedAssetFinder(ILogger<UnturnedAssetFinder> logger)
+    {
+        Interlocked.Increment(ref _semaphoreQueue);
+
+        // nexus is needed to get the type dictionary that unturned uses for v1 assets.
+        UnturnedNexus? old = Interlocked.CompareExchange(ref _nexus, new UnturnedNexus(), null);
+        if (old == null)
+        {
+            lock (_nexus)
+            {
+                try
+                {
+                    _nexus.initialize();
+                }
+                catch (ArgumentException)
+                {
+                    // ignored
+                }
+            }
+        }
+
+        _logger = logger;
+    }
+    public void Dispose()
+    {
+        int diposed = Interlocked.Exchange(ref _disposed, 1);
+        if (diposed == 0)
+            return;
+
+        int val = Interlocked.Decrement(ref _semaphoreQueue);
+        Interlocked.CompareExchange(ref _semaphoreQueue, 0, -1);
+        if (val <= 0)
+        {
+            UnturnedNexus? nexus = Interlocked.Exchange(ref _nexus, null);
+            if (nexus != null)
+            {
+                lock (nexus)
+                    nexus.shutdown();
+            }
+        }
+    }
+    public async Task<List<UnturnedAssetFile>> ScanAsync(string folder, string language = "English", CancellationToken token = default)
+    {
+        if (_disposed > 0 || _nexus == null)
+            throw new ObjectDisposedException(nameof(UnturnedAssetFinder));
+        List<UnturnedAssetFile> rtn = new List<UnturnedAssetFile>(128);
+        List<Task> waiting = new List<Task>(512);
+        DirectoryInfo dir = new DirectoryInfo(folder);
+        foreach (FileInfo info in dir.EnumerateFiles("*", SearchOption.AllDirectories))
+        {
+            // looks for a lone asset file
+            if (info.Extension.Equals(".asset", StringComparison.Ordinal))
+            {
+                string? dirName = Path.GetFileName(Path.GetDirectoryName(info.FullName));
+                if (dirName != null && !dirName.Equals(Path.GetFileNameWithoutExtension(info.FullName), StringComparison.Ordinal))
+                {
+                    if (File.Exists(Path.Combine(dirName, dirName + ".asset")) ||
+                        File.Exists(Path.Combine(dirName, dirName + ".dat")) ||
+                        File.Exists(Path.Combine(dirName, "Asset.dat")))
+                        continue;
+                }
+            }
+            // looks for a dat file in its folder
+            else if (info.Extension.Equals(".dat", StringComparison.Ordinal))
+            {
+                string? dirName = Path.GetFileName(Path.GetDirectoryName(info.FullName));
+                if (dirName != null && !dirName.Equals(Path.GetFileNameWithoutExtension(info.FullName), StringComparison.Ordinal))
+                    continue;
+            }
+            else continue;
+            waiting.Add(Task.Run(async () =>
+            {
+                DatDictionary? dict = await ReadFileAsync(info.FullName, token).ConfigureAwait(false);
+                if (dict == null)
+                    return;
+                
+                UnturnedAssetFile? asset = TryRead(dict, info);
+                if (asset != null)
+                {
+                    // ReSharper disable once InconsistentlySynchronizedField
+                    await asset.CacheLocalAsync(_parser, token).ConfigureAwait(false);
+                    lock (rtn)
+                        rtn.Add(asset);
+                }
+            }, token));
+        }
+
+        await Task.WhenAll(waiting);
+
+        _logger.LogDebug($"Assets discovered: {rtn.Count}.");
+        lock (rtn)
+        {
+            return rtn;
+        }
+    }
+    public List<UnturnedAssetFile> Scan(string folder, string language = "English")
+    {
+        if (_disposed > 0 || _nexus == null)
+            throw new ObjectDisposedException(nameof(UnturnedAssetFinder));
+        List<UnturnedAssetFile> rtn = new List<UnturnedAssetFile>(32);
+        DirectoryInfo dir = new DirectoryInfo(folder);
+        foreach (FileInfo info in dir.EnumerateFiles("*", SearchOption.AllDirectories))
+        {
+            // looks for a lone asset file
+            if (info.Extension.Equals(".asset", StringComparison.Ordinal))
+            {
+                string? dirName = Path.GetFileName(Path.GetDirectoryName(info.FullName));
+                if (dirName != null && !dirName.Equals(Path.GetFileNameWithoutExtension(info.FullName), StringComparison.Ordinal))
+                {
+                    if (File.Exists(Path.Combine(dirName, dirName + ".asset")) ||
+                        File.Exists(Path.Combine(dirName, dirName + ".dat")) ||
+                        File.Exists(Path.Combine(dirName, "Asset.dat")))
+                        continue;
+
+                }
+            }
+            // looks for a dat file in its folder
+            else if (info.Extension.Equals(".dat", StringComparison.Ordinal))
+            {
+                string? dirName = Path.GetFileName(Path.GetDirectoryName(info.FullName));
+                if (dirName != null && !dirName.Equals(Path.GetFileNameWithoutExtension(info.FullName), StringComparison.Ordinal))
+                    continue;
+            }
+            else continue;
+            DatDictionary? dict = ReadFile(info.FullName);
+            if (dict == null)
+                continue;
+            UnturnedAssetFile? asset = TryRead(dict, info, language);
+            if (asset != null)
+            {
+                // ReSharper disable once InconsistentlySynchronizedField
+                asset.CacheLocal(_parser);
+                rtn.Add(asset);
+            }
+        }
+
+        _logger.LogDebug($"Assets discovered: {rtn.Count}.");
+        return rtn;
+    }
+    public UnturnedAssetFile? TryRead(DatDictionary dictionary, FileInfo file, string language = "English")
+    {
+        if (_disposed > 0 || _nexus == null)
+            throw new ObjectDisposedException(nameof(UnturnedAssetFinder));
+        lock (_nexus)
+        {
+            Guid guid;
+            ushort id = 0;
+            Type? assetType = null;
+            // v2 metadata
+            if (dictionary.TryGetDictionary("Metadata", out DatDictionary metadata))
+            {
+                if (!metadata.TryParseGuid("Guid", out guid))
+                {
+                    ReportError(file, "Missing Metadata.Guid property.");
+                    return null;
+                }
+                assetType = metadata.ParseType("Type");
+                if (assetType == null)
+                {
+                    ReportError(file, "Missing Metadata.Type property.");
+                    return null;
+                }
+            }
+            else
+            {
+                // v1 metadata
+                dictionary.TryParseGuid("GUID", out guid);
+                dictionary.TryParseUInt16("ID", out id);
+            }
+            if (assetType == null)
+            {
+                // v1 metadata
+                if (!dictionary.TryGetString("Type", out string typeStr))
+                {
+                    ReportError(file, "Missing Type property.");
+                    return null;
+                }
+
+                assetType = Assets.assetTypes.getType(typeStr);
+
+                if (assetType == null)
+                {
+                    assetType = dictionary.ParseType("Type");
+
+                    if (assetType == null)
+                    {
+                        ReportError(file, $"Unknown value: \"{typeStr}\" for Type property.");
+                        return null;
+                    }
+                }
+            }
+
+            if (!typeof(Asset).IsAssignableFrom(assetType))
+            {
+                ReportError(file, $"Type {assetType.FullName} is not assignable from Asset.");
+                return null;
+            }
+
+            UnturnedAssetFile asset = new UnturnedAssetFile(assetType, file, guid, id, dictionary, language);
+            return asset;
+        }
+    }
+    private void ReportError(FileInfo file, string message)
+    {
+        _logger.LogWarning($"Asset \"{Path.GetFileNameWithoutExtension(file.FullName)}\" load error: \"{message}\".{Environment.NewLine}\tFile: {file.FullName}");
+    }
+
+    private DatDictionary? ReadFile(string path)
+    {
+        using FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+        if (fileStream.Length > MaxFileSize)
+        {
+            return null;
+        }
+
+        using StreamReader inputReader = new StreamReader(fileStream);
+
+        lock (_parser)
+            return _parser.Parse(inputReader);
+    }
+    private async Task<DatDictionary?> ReadFileAsync(string path, CancellationToken token = default)
+    {
+#if NETCOREAPP || NETSTANDARD
+        await using FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+#else
+        using FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+#endif
+        if (fileStream.Length > MaxFileSize)
+        {
+            return null;
+        }
+
+        using MemoryStream stream = new MemoryStream((int)fileStream.Length);
+        await fileStream.CopyToAsync(stream, 81920, token).ConfigureAwait(false);
+        stream.Seek(0L, SeekOrigin.Begin);
+
+        using StreamReader inputReader = new StreamReader(stream);
+
+        lock (_parser)
+            return _parser.Parse(inputReader);
+    }
+}
